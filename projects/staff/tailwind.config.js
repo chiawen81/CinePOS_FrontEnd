30306a9da13e9555aadd40e99a8732a03a7cf1b7
labibi.lg <labibi.lg@gmail.com>
2023-04-24 17:35:34 +0800

    feat: 調整/新增tailwind

--- projects/staff/tailwind.config.js
+++ projects/staff/tailwind.config.js
@@ -6,15 +6,17 @@ module.exports = {
   purge: {
     enabled: true, // 每次編譯都觀測(若沒寫預設false則只會在正式機時才會偵測)
     content: [ // 偵測哪幾種檔案類型，其餘的會捨去
-      "projects/staff/src/**/*.html",
-      "projects/staff/src/**/*.scss"
+      "projects/staff/src/**/*.{html,ts,scss}",
+      "projects/share-libs/src/**/*.{html,ts,scss}",
     ],
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       colors: {
-        primary: '#8988FF'
+        primary: '#8988FF',
+        secondary: '#222222',
+        dark: '#151515'
       }
     },
   },
