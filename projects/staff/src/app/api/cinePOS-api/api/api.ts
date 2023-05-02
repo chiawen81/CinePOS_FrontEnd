64d590a137058defcf93274b72c8ee848781d0bf
labibi.lg <labibi.lg@gmail.com>
2023-05-02 15:39:33 +0800

    feat: 刪除舊的資訊

--- projects/staff/src/app/api/cinePOS-api/api/api.ts
+++
@@ -1,5 +0,0 @@
-export * from './manager.service';
-import { ManagerService } from './manager.service';
-export * from './staff.service';
-import { StaffService } from './staff.service';
-export const APIS = [ManagerService, StaffService];
--- projects/staff/src/app/api/cinePOS-api/model/models.ts
+++
@@ -1,7 +0,0 @@
-export * from './commonResFailed';
-export * from './infoUpdateReq';
-export * from './infoUpdateRes';
-export * from './infoUpdateResData';
-export * from './loginReq';
-export * from './loginRes';
-export * from './loginResData';
