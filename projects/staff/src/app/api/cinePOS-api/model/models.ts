cb01fcf5fe2b14c5238b281f96377d2f6795c4dd
labibi.lg <labibi.lg@gmail.com>
2023-05-02 15:43:19 +0800

    feat: 在更新一次api接口

---
+++ projects/staff/src/app/api/cinePOS-api/model/models.ts
@@ -0,0 +1,7 @@
+export * from './commonResFailed';
+export * from './infoUpdateReq';
+export * from './infoUpdateRes';
+export * from './infoUpdateResData';
+export * from './loginReq';
+export * from './loginRes';
+export * from './loginResData';
