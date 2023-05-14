b52746571d14bd7b428866c21174df9a55307652
chiawen81 <fangchiawen@gmail.com>
2023-05-15 00:21:41 +0800

    更新後台環境檔、佈版到github page

--- projects/manager/src/environments/environment.prod.ts
+++ projects/manager/src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
 export const environment = {
+  channel: 'CinePOS_FrontEnd',
   appVersion: require('package.json').version + '-prod',
   production: true,
   DEFAULT_IP: '',
+  cinePosApi: "https://api.cine-pos.com"
 };
