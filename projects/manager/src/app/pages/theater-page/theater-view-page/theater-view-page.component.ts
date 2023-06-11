f297444f4789dc04ae60b162ac069709e4cc9dd7
Dee <deewangatwork@gmail.com>
2023-06-11 22:34:13 +0800

    feat: 檢視影廳頁

---
+++ projects/manager/src/app/pages/theater-page/theater-view-page/theater-view-page.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theater-view-page',
+  templateUrl: './theater-view-page.component.html',
+  styleUrls: ['./theater-view-page.component.scss']
+})
+export class TheaterViewPageComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+}
