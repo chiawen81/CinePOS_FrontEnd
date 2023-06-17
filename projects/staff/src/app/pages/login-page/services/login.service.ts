807e56f73232ebad537a539a1c296c57182f1232
LABIBI-LG <labibi.lg@gmail.com>
2023-06-17 16:47:15 +0800

    fix: 員工資訊轉打另一隻API

--- projects/staff/src/app/pages/login-page/services/login.service.ts
+++ projects/staff/src/app/pages/login-page/services/login.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { filter, Observable, tap } from 'rxjs';
-import { LoginRes, StaffService } from '../../../api/cinePOS-api';
+import { LoginRes, StaffService, UserProfileRes } from '../../../api/cinePOS-api';
 import { StorageEnum } from '../../../core/enums/storage/storage-enum';
 import { StorageService } from '../../../core/services/storage/storage.service';
 import { ProfileData } from '../../../core/interface/profile-data';
@@ -15,6 +15,7 @@ export class LoginService {
     private storageService: StorageService
   ) { }
 
+  /**登入請求 */
   v1StaffLoginPost$(LoginReq: { staffId: string, password: string }): Observable<LoginRes> {
     return this.staffService.v1StaffLoginPost(LoginReq)
       .pipe(
@@ -22,13 +23,25 @@ export class LoginService {
         filter(res => res.code === 1),
         tap((res) => {
           this.storageService.setLocalStorage(StorageEnum.token,res.data!.token);
+        })
+      )
+  }
+  /**取得櫃台人員資料 */
+  v1StaffUserProfileStaffIdGet(staffId: string): Observable<UserProfileRes> {
+    return this.staffService.v1StaffUserProfileStaffIdGet(staffId)
+      .pipe(
+        tap(res => res.code !== 1 && alert(res.message)),
+        filter(res => res.code === 1),
+        tap((res) => {
           const profileData:ProfileData = {
-            name: res.data!.name!,
+            name: res.data?.name!,
             staffId: res.data!.staffId!,
-            imgUrl: 'assets/images/angular-icon.webp'
+            imgUrl: !!res.data!.stickerUrl! ?  res.data!.stickerUrl!:'assets/images/angular-icon.webp'
           }
           this.storageService.setLocalStorage(StorageEnum.profileData,profileData);
         })
       )
+
   }
+
 }
